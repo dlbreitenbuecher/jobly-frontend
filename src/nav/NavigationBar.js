8286edd40cbd5ec46e239e204912b5d654d81cac
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2020-11-08 21:00:07 -0800

    Added Hamburger menu to Navbar
    Hamburger menu now displays when the page width is less than 768px. Updated the docstring

--- src/nav/NavigationBar.js
+++ src/nav/NavigationBar.js
@@ -1,13 +1,24 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { NavLink, Link } from "react-router-dom";
 import './NavigationBar.css'
 import CurrentUserContext from "../auths/CurrentUserContext";
-import { Container } from 'reactstrap';
+import { Collapse, Container, NavbarToggler } from 'reactstrap';
 
 /**Renders Nav Links based on whether user is loggedin or not
  * 
  * Accepts props:
  * - currentUser -> details about currentUser
+ * 
+ * State:
+ *  - collapsed:
+ *      Determines whether or not to display items in the hamburger menu.
+ *        Elements of the navbar convert to a hamburger menu when page width
+ *        is < 768 px
+ * 
+ * Contest:
+ * - CurrentUserContext
+ *      Used to determine if user is logged in or not
+ *       
  * 
  * If user is loggedIn, renders link to:
  *    -Companies
@@ -25,15 +36,20 @@ import { Container } from 'reactstrap';
  * App -> NavBar -> (links)
  */
 function NavigationBar({ logout }) {
+  const [collapsed, setCollapsed] = useState(true);
   const { currentUser } = useContext(CurrentUserContext)
   console.log('in NavBar. CurrentUser:', currentUser);
+
+  function toggleNavbar() {
+    setCollapsed(!collapsed);
+  }
 
   function showLoggedinOrSignupNavs() {
     if (!currentUser) {
       return (
         <ul className="navbar-nav ml-auto">
           <li className='nav-item mr-4'>
-            <NavLink to="/login" className='nav-link'>login</NavLink>
+            <NavLink to="/login" className='nav-link'>Login</NavLink>
           </li>
           <li className='nav-item mr-4'>
             <NavLink to="/signup" className='nav-link'>Signup</NavLink>
@@ -42,20 +58,20 @@ function NavigationBar({ logout }) {
       )
     } else {
       return (
-        <ul className="navbar-nav ml-auto">
-          <li className='nav-item mr-4'>
-            <NavLink to="/companies" className='nav-link'>Companies</NavLink>
-          </li>
-          <li className='nav-item mr-4'>
-            <NavLink to="/jobs" className='nav-link'>Jobs</NavLink>
-          </li>
-          <li className='nav-item mr-4'>
-            <NavLink to="/profile" className='nav-link'>Profile</NavLink>
-          </li>
-          <li className='nav-item mr-4'>
-            <Link onClick={logout} to="/" className='nav-link'>Log out {currentUser.username}</Link>
-          </li>
-        </ul>
+          <ul className="navbar-nav ml-auto">
+            <li className='nav-item mr-4'>
+              <NavLink to="/companies" className='nav-link'>Companies</NavLink>
+            </li>
+            <li className='nav-item mr-4'>
+              <NavLink to="/jobs" className='nav-link'>Jobs</NavLink>
+            </li>
+            <li className='nav-item mr-4'>
+              <NavLink to="/profile" className='nav-link'>Profile</NavLink>
+            </li>
+            <li className='nav-item mr-4'>
+              <Link onClick={logout} to="/" className='nav-link'>Log out {currentUser.username}</Link>
+            </li>
+          </ul>
       )
     }
   }
@@ -65,7 +81,14 @@ function NavigationBar({ logout }) {
       <NavLink exact to="/" className='navbar-brand'>
         Jobly
       </NavLink>
+
+      {/* Hamburger Menu */}
+      <NavbarToggler onClick={toggleNavbar} className="mr-2 navbar-light" />
+      <Collapse isOpen={!collapsed} navbar>
+
       {showLoggedinOrSignupNavs()}
+    
+    </Collapse>
     </nav>
   )
 }
